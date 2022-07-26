17cb4e52c34c65a1f5299dcf514d7f38ff88e5c3
Kimyerim1935 <kimyerim1935@naver.com>
2022-07-26 19:12:02 +0900

    mod: todo 리스트 생성

--- src/main/java/NodeList.java
+++ src/main/java/NodeList.java
@@ -1,9 +1,6 @@
 import java.util.*;
 
-public class NodeList {
-    public static void main(String[] args){
-        int index; //리스트의 순서
-        List<String> testList = new ArrayList<String>(); // js에서 array.push / array.pop 코드가 있는데 자바에도 있나? 싶어서 찾아봤더니 나온 코드
+//Todo
 
 //       boolean add() {} // 리스트 가장 끝에 항목을 추가
 //       void add(int index) {} // 리스트의 index에 해당하는 부분에 항목을 추가
@@ -18,6 +15,11 @@ public class NodeList {
 //       int size()                         // 현재 리스트의 사이즈를 반환
 //       String get(int index)              // 리스트의 index에 해당하는 항목을 추출하며, 제거는 하지 않음
 //       void clear()                       // 현재 리스트를 빈 리스트로 초기화
+
+public class NodeList {
+    public static void main(String[] args){
+        int index; //리스트의 순서
+        List<String> testList = new ArrayList<String>(); // js에서 array.push / array.pop 코드가 있는데 자바에도 있나? 싶어서 찾아봤더니 나온 코드
 
     }
 }
