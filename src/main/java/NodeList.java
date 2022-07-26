dd318f41477bc5f8f05f19535ecd8f2c429cc657
Kimyerim1935 <kimyerim1935@naver.com>
2022-07-26 19:10:00 +0900

    add: 단방향 연결리스트 구현 하기 전, 클래스, 변수 선언

---
+++ src/main/java/NodeList.java
@@ -0,0 +1,23 @@
+import java.util.*;
+
+public class NodeList {
+    public static void main(String[] args){
+        int index; //리스트의 순서
+        List<String> testList = new ArrayList<String>(); // js에서 array.push / array.pop 코드가 있는데 자바에도 있나? 싶어서 찾아봤더니 나온 코드
+
+//       boolean add() {} // 리스트 가장 끝에 항목을 추가
+//       void add(int index) {} // 리스트의 index에 해당하는 부분에 항목을 추가
+//       void addFirst() {} // 리스트의 가장 끝에 항목을 추가
+//       void addLast(String element)       // 리스트의 가장 끝에 항목을 추가
+//
+//       String remove()                    // 리스트의 가장 끝에서 항목을 추출 및 제거
+//       boolean remove(String element)     // 리스트에서 해당 항목을 찾아 제거
+//       String removeFirst()               // 리스트의 가장 처음 항목을 추출 및 제거
+//       String removeList()                // 리스트의 가장 끝 항목을 추출 및 제거
+//
+//       int size()                         // 현재 리스트의 사이즈를 반환
+//       String get(int index)              // 리스트의 index에 해당하는 항목을 추출하며, 제거는 하지 않음
+//       void clear()                       // 현재 리스트를 빈 리스트로 초기화
+
+    }
+}
