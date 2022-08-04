756fbcf8e64b57e170eb3b7d996e719e9517239d
doRnsns98 <doRnsns98@blockodyssey.io>
2022-08-04 09:25:09 +0900

    mod: commit 테스트

--- src/main/java/Application.java
+++ src/main/java/Application.java
@@ -3,16 +3,13 @@ class Application {
         NodeList nodeList = new NodeList(); // (first: null, size = 0)
 
         nodeList.add("aaa"); // (first: [data: AAA, next: null], size = 1)
-//        nodeList.add("bbb"); // (first: [data: AAA, next: [data: BBB, next: null]], size = 2)
-//        nodeList.add("ccc");
-//        nodeList.add("ddd");
-//        nodeList.add("eee");
+        nodeList.add("bbb"); // (first: [data: AAA, next: [data: BBB, next: null]], size = 2)
+        nodeList.add("ccc");
+        nodeList.add("ddd");
+        nodeList.add("eee");
 
         System.out.println("remove= " + nodeList.remove());
         System.out.println("size= " + nodeList.size());
-//        System.out.println("remove= " + nodeList.remove());
-//        System.out.println("size= " + nodeList.size());
-//        System.out.println("remove= " + nodeList.remove());
-//        System.out.println("size= " + nodeList.size());
+
     }
 }
