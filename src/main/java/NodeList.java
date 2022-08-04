4ba85aa5114e0f4302aef0526d70ac85264999a0
doRnsns98 <doRnsns98@blockodyssey.io>
2022-08-04 09:22:28 +0900

    mod: remove 함수 코드리뷰 후 수정완료

--- src/main/java/NodeList.java
+++ src/main/java/NodeList.java
@@ -1,6 +1,5 @@
-//Todo
+// Todo
 
-//       boolean add() {} // 리스트 가장 끝에 항목을 추가
 //       void add(int index) {} // 리스트의 index에 해당하는 부분에 항목을 추가
 //       void addFirst() {} // 리스트의 가장 끝에 항목을 추가
 //       void addLast(String element)       // 리스트의 가장 끝에 항목을 추가
@@ -10,93 +9,116 @@
 //       String removeFirst()               // 리스트의 가장 처음 항목을 추출 및 제거
 //       String removeList()                // 리스트의 가장 끝 항목을 추출 및 제거
 //
-//       int size()                         // 현재 리스트의 사이즈를 반환
-//       String get(int index)              // 리스트의 index에 해당하는 항목을 추출하며, 제거는 하지 않음
 //       void clear()                       // 현재 리스트를 빈 리스트로 초기화
 
 class Node {
-    String data; // 인스턴스변수 , 멤버변수
-    Node next;
+  String data; // 인스턴스변수 , 멤버변수
+  Node next;
 
-    // 생성자(Constructor)
-    // 인스턴스 생성시에 호출되는 특별한 메서드
-    public Node(String element) {
-        this.data = element;
-        this.next = null;
-    }
+  // 생성자(Constructor)
+  // 인스턴스 생성시에 호출되는 특별한 메서드
+  public Node(String element) {
+    this.data = element;
+    this.next = null;
+  }
 
-    // 메서드
-    String getData() {
-        System.out.println(this.data);
-        return this.data;
-    }
-
+  // 메서드
+  String getData() {
+    //        System.out.println(this.data);
+    return this.data;
+  }
 }
 
 class NodeList {
-    Node first;
-    Node last;
-    int size;
+  Node first;
+  Node last;
+  int size;
 
-    public NodeList() {
-        this.first = null;
-        this.last = null;
-        this.size = 0;
+  public NodeList() {
+    this.first = null;
+    this.last = null;
+    this.size = 0;
+  }
+
+  // 리스트 추가 함수
+  boolean add(String element) {
+    Node node = new Node(element); // 노드 생성
+
+    if (first == null) {
+      first = node;
+    } else {
+      last.next = node;
+    }
+    last = node;
+    size++; // index of NodeList
+    return true;
+  }
+
+  public String get(int index) {
+    Node cursor = first;
+
+    if (index < 0 || index > size - 1) {
+      String empty = "Empty";
+      return empty;
     }
 
-    // 리스트 추가 함수
-    boolean add(String element) {
-        Node node = new Node(element); // 노드 생성
-
-        if (first == null) {
-            first = node;
-        } else {
-            last.next = node;
-        }
-        last = node;
-        size++; // index of NodeList
-        return true;
+    for (int i = 0; i < index; i++) {
+      cursor = cursor.next;
     }
 
-    public String get(int index) {
-        Node cursor = first;
+    return cursor.data;
+  }
 
-        if (index < 0 || index > size - 1) {
-            String empty = "Empty";
-            return empty;
-        }
+  //    public String remove() {
+  //        if (size == 0) {
+  //            return "삭제할 노드가 없습니다.";
+  //        }
+  //        Node cursor = first;
+  //        String result = last.getData();
+  //        for(int i = 0; i < size - 2; i++) {
+  //            cursor = cursor.next;
+  //            if (i == size-2) {
+  //                cursor.next = null; // Error
+  //            }
+  //        }
+  //        System.out.println("cursor= " + cursor.data + cursor.next);
+  //
+  //        last = cursor;
+  //        size--;
+  //        System.out.println("last= " + last.data + last.next);
+  //        return result;
+  //    }
 
-        for (int i = 0; i < index; i++) {
-            cursor = cursor.next;
-        }
+  // 가장 마지막 항목 제거 함수
+  public String remove() {
+    if (size == 0) {
+      return "삭제할 노드가 없습니다.";
+    }
+    size--;
 
-        return cursor.data;
+    if (first == last) {
+      String result = first.getData();
+      first = last = null;
+      return result;
     }
 
-    public String remove() {
-        if (size == 0) {
-            return "삭제할 노드가 없습니다.";
-        }
+    Node cursor = first;
 
-        //                cursor
-        // first          last
-        // aaa     ->      bbb      ->      null     ->      null
-        // size : 3
-        Node cursor = first;
-        String result = last.getData();
-        for(int i = 0; i < size - 2; i++) {
-            cursor = cursor.next;
-            cursor.next = null; // Error
-        }
-        last = cursor;
-        size--;
-        return result;
+    String result = last.getData();
+
+    while (cursor.next != last) {
+      cursor = cursor.next;
     }
-    
-    // 리스트 사이즈 추출 함수
-    int size() {
-        if (size == 0) System.out.println("Node를 추가해주세요.");
-//        System.out.println(size);
-        return size;
-    }
+
+    cursor.next = null;
+    last = cursor;
+
+    return result;
+  }
+
+  // 리스트 사이즈 추출 함수
+  int size() {
+    //        System.out.println(size);
+    return size;
+  }
 }
