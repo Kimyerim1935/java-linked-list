30cd25265dec4176b05b7b1bff033565b92b0344
Kimyerim1935 <Kimyerim1935@naver.com>
2022-08-05 00:06:39 +0900

    mod: remove first구현, clear/indexAdd 함수 구현중

--- src/main/java/Application.java
+++ src/main/java/Application.java
@@ -2,24 +2,20 @@ class Application {
     public static void main(String[] args) {
         NodeList nodeList = new NodeList(); // (first: null, size = 0)
 
-//        nodeList.add("aaa"); // (first: [data: AAA, next: null], size = 1)
-//        nodeList.add("bbb"); // (first: [data: AAA, next: [data: BBB, next: null]], size = 2)
-//        nodeList.add("ccc");
-//        nodeList.add("ddd");
-//        nodeList.addLast("eee");
-//        nodeList.addLast("fff");
-//        nodeList.addLast("ggg");
-//        nodeList.addLast("hhh");
-        nodeList.addFirst("aaa");
-        nodeList.addFirst("bbb");
-        nodeList.addFirst("ccc");
-        nodeList.addFirst("ddd");
+//        nodeList.addFirst("aaa");
+//        nodeList.addFirst("bbb");
+//        nodeList.addFirst("ccc");
+//        nodeList.addFirst("ddd");
         nodeList.addLast("eee");
         nodeList.addLast("fff");
         nodeList.addLast("ggg");
         nodeList.addLast("hhh");
 
+//        nodeList.add(1, "111");
+//        nodeList.add(3, "333");
+//        nodeList.removeFirst();
         nodeList.print();
+
 //        System.out.println("remove= " + nodeList.remove());
 //        System.out.println("size= " + nodeList.size());
 //        System.out.println("remove= " + nodeList.remove());
--- src/main/java/NodeList.java
+++ src/main/java/NodeList.java
@@ -3,7 +3,6 @@
 //       void add(int index) {} // 리스트의 index에 해당하는 부분에 항목을 추가
 //
 //       boolean remove(String element)     // 리스트에서 해당 항목을 찾아 제거
-//       String removeFirst()               // 리스트의 가장 처음 항목을 추출 및 제거
 //
 //       void clear()                       // 현재 리스트를 빈 리스트로 초기화
 
@@ -20,7 +19,6 @@ class Node {
 
   // 메서드
   String getData() {
-    //        System.out.println(this.data);
     return this.data;
   }
 }
@@ -77,6 +75,40 @@ class NodeList {
     }
   }
 
+  // 리스트의 index에 해당하는 부분에 항목을 추가
+  void add(int index, String element) {
+    // index번째의 data 추가
+    // 이전 노드.next에 추가 노드 주소 연결, 추가 노드.next에 원래노드.next 주소 넣기
+    // size가 0이라면 메시지 출력 후 first, last에 추가
+    // aaa bbb(ccc) [element, ccc] ccc
+
+    Node indexNode = new Node(element);
+    Node cursor = first;
+
+//    if (size == 0){
+//      System.out.println("생성된 노드가 없으므로, 첫번째에 항목을 추가합니다.");
+//      first = indexNode;
+//      last = indexNode;
+//    } else {
+
+    // 지금 노드 리스트 상태
+    // f            l
+    // eee fff ggg hhh
+    // fff ggg hhh null
+    // 만약 i번째에 추가하고 싶다면, 커서를 i-1번째로 옮긴 뒤, next를 element로
+    // 커서의 next를 element.next로
+
+        int move = index-2;
+
+        for (int i = 0; i < move; i++){
+          cursor = cursor.next;
+        }
+//    }
+    indexNode.next = cursor.next;
+    cursor.next = indexNode;
+    size++;
+  }
+
   // 리스트의 가장 끝에 항목을 추가
   void addLast(String element) {
     Node lastAddNode = new Node(element);
@@ -88,7 +120,6 @@ class NodeList {
     }
     size++;
     last = lastAddNode;
-    System.out.println("lastNode ====> " + last.getData());
   }
 
   public String get(int index) {
@@ -133,7 +164,25 @@ class NodeList {
     return result;
   }
 
-  // 리스트의 가장 끝 항목을 추출 및 제거
+  // 리스트의 가장 첫번째 항목을 추출 및 제거
+  String removeFirst() {
+
+    if (first == last) {
+      String result = first.getData();
+      first = last = null;
+      return result;
+    }
+    Node cursor = first;
+    String result = first.data;
+
+    cursor.data = null;
+    first = cursor.next;
+    size--;
+
+    return result;
+  }
+
+  // 리스트의 가장 마지막 항목을 추출 및 제거
   String removeList() {
     return remove();
   }
@@ -142,5 +191,11 @@ class NodeList {
   int size() {
     //        System.out.println(size);
     return size;
+  }
+
+  // 리스트 초기화 함수
+  void clear() {
+    Node cursor = first;
+
   }
 }
